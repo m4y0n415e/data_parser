31256fba584a1aa2877a4d6aea648b6071d8f6b1
Maja Rudnicka <rud.m1011@gmail.com>
2026-08-26 15:45:41 +0200

    Change the timeline.py script to employ universal strategy for knee point calculation

--- timelines.py
+++ timelines.py
@@ -3,12 +3,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PowerTransformer
 from sklearn import set_config
 from sklearn.mixture import GaussianMixture
 from statsmodels.tsa.ar_model import AutoReg
 from encoding import detect_encoding
 from scipy.stats import norm
+from kneed import KneeLocator, DataGenerator
 
 def load(input):
     coding = detect_encoding(input)
@@ -18,25 +19,25 @@ def load(input):
             print("File not found.")
     return df
 
-def draw_graphs(scaled_df, df_filtered):
+def draw_graphs(df_ldct, df_filtered):
     # initializing the KMeans model to assign clusters for every group - draft version
-    kmeans = KMeans(init='random', n_clusters=3, n_init=10, random_state=1)
-    kmeans.fit(scaled_df)
+    # kmeans = KMeans(init='random', n_clusters=3, n_init=10, random_state=1)
+    # kmeans.fit(scaled_df)
 
-    df_filtered['cluster_id'] = pd.Series(kmeans.labels_)
-    point_in_time = (df_filtered['days_since_ldct_visit'].astype(float).groupby(df_filtered['cluster_id'])).mean()
-    df_filtered['days_since_ldct_visit'] = pd.to_numeric(df_filtered['days_since_ldct_visit'], errors='coerce')
-    frequencies = df_filtered['cluster_id'].value_counts(sort=False).sort_index(ascending=True)
+    # df_filtered['cluster_id'] = pd.Series(kmeans.labels_)
+    # point_in_time = (df_filtered['days_since_ldct_visit'].astype(float).groupby(df_filtered['cluster_id'])).mean()
+    # df_filtered['days_since_ldct_visit'] = pd.to_numeric(df_filtered['days_since_ldct_visit'], errors='coerce')
+    # frequencies = df_filtered['cluster_id'].value_counts(sort=False).sort_index(ascending=True)
 
     # v-line plot of the cluster centers
-    plt.vlines(x=point_in_time, ymin=0, ymax=frequencies.astype(float).mean())
+    # plt.vlines(x=point_in_time, ymin=0, ymax=frequencies.astype(float).mean())
 
-    plt.title("Patient visits with k-means temporal nodes")
-    plt.ylabel("Frequency")
-    plt.xlabel("Time cluster of the visits")
+    # plt.title("Patient visits with k-means temporal nodes")
+    # plt.ylabel("Frequency")
+    # plt.xlabel("Time cluster of the visits")
     
-    plt.savefig(R"graphs/kmeans_interval_lines.png")
-    plt.clf()
+    # plt.savefig(R"graphs/kmeans_interval_lines.png")
+    # plt.clf()
 
     # kde plot of the days clusters (concentrated around 0, 6 mo, and 12 mo timestamps)
     x = df_ldct['days_since_initial_visit_in_ldct'].astype(float)
@@ -47,34 +48,68 @@ def draw_graphs(scaled_df, df_filtered):
     plt.clf()
     
 
-def knee_point_calculation():
-    return 0
+def calculate_bic_for_gmm(X, max_clusters):
+    bic_values = []
+    for n in range(1, max_clusters + 1):
+        gmm = GaussianMixture(n_components=n, random_state=0).fit(X)
+        bic_values.append(gmm.bic(X))
+
+    # creating a BIC plot to visualize the "knee" point
+    plt.plot(range(1,12), bic_values, marker='o')
+    plt.title('BIC values')
+    plt.xlabel('N-components')
+    plt.ylabel('BIC')
+    plt.show()
+    plt.clf() 
+
+    return bic_values
 
 
-def create_GMM(df_ldct):
+def knee_point_calculation(max_clusters, bic_values):
+    # kneedle = KneeLocator(range(1, max_clusters + 1), bic_values, S=20, curve='convex', direction='decreasing')        
+    # knees.append(kneedle.knee)
+    # norm_knees.append(kneedle.norm_knee)
+    
+    diff_table = np.diff(bic_values, n=1)
+    minimum = np.amin(diff_table)
+
+    threshold = 0.01 * minimum
+    knee_point = 0
+    for index in range(1, max_clusters - 1):
+        if (diff_table[index] > threshold):
+            knee_point = index + 1
+            break
+    # print(knee_point)
+    return knee_point
+
+
+def create_GMM(df_ldct, optimal_k): # continue changes from here - making the code universal for the gmm model, with the indexes below
     # creating the GMM for 4 components (based on the BIC score)
     X = df_ldct['days_since_initial_visit_in_ldct'].astype(float).values.reshape(-1,1)
-    model_4 = GaussianMixture(n_components=4, random_state=0, n_init=10)
-    model_4.fit(X)
+    model = GaussianMixture(n_components=optimal_k, random_state=0, n_init=10)
+    model.fit(X)
 
     # sorting the indexes into a proper sequence
-    order = model_4.means_
+    order = model.means_
     flat_means = order.flatten()
     sorted_indexes = np.argsort(flat_means)
 
     # sorting the means, covariances and weights
-    sorted_means = model_4.means_.flatten()[sorted_indexes]
-    sorted_covs = model_4.covariances_.flatten()[sorted_indexes]
-    sorted_weights = model_4.weights_.flatten()[sorted_indexes]
+    sorted_means = model.means_.flatten()[sorted_indexes]
+    sorted_covs = model.covariances_.flatten()[sorted_indexes]
+    sorted_weights = model.weights_.flatten()[sorted_indexes]
 
+    # calculating the standard deviation and the "timeline"
     st_dev = np.sqrt(sorted_covs)
     timeline = np.linspace(0, sorted_means[3] + 3 * st_dev[3], 1000)
-    timeline_2d = timeline.reshape(-1, 1)
+    timeline_2d = timeline.reshape(-1, 1) # needs evaluation; why 3 ?
 
-    probability_matrix_np = model_4.predict_proba(timeline_2d)[:, sorted_indexes] 
+    probability_matrix_np = model.predict_proba(timeline_2d)[:, sorted_indexes] 
     # predict_proba basically uses PDF to determine probability of an x belonging to a Gaussian function n
 
     max_el = []
+    
+    # code below needs rewriting to be universal for different data sets
 
     # creating the probability matrix for each components - which values fit best into a component Gaussian
     delta_01 = probability_matrix_np[:, 1] - probability_matrix_np[:, 0]
@@ -147,13 +182,28 @@ if __name__ == "__main__":
     df_filtered = df_ldct[no_in_seq].copy()
     df_filtered.fillna({'days_since_ldct_visit': 0}, inplace=True)
 
-    scaled_df = StandardScaler().fit_transform(df_filtered['days_since_ldct_visit'].to_frame())
+    draw_graphs(df_ldct, df_filtered)
 
-    draw_graphs(scaled_df, df_filtered)
+    x_data = df_ldct['days_since_initial_visit_in_ldct'].astype(float)
 
-    # optimal_k = knee_point_calculation()
+    # setting the threshold for BIC score
+    threshold = x_data.quantile(0.95)
+
+    clean_data = x_data[x_data <= threshold]
+
+    X_data = clean_data.values.reshape(-1,1)
+
+    transformer = PowerTransformer(method='yeo-johnson')
+
+    transformed_X_data = transformer.fit_transform(X_data)
+
+    max_clusters = 11
+
+    bic_values = calculate_bic_for_gmm(transformed_X_data, max_clusters)
+
+    optimal_k = knee_point_calculation(max_clusters, bic_values)
    
-    create_GMM(df_ldct)
+    create_GMM(df_ldct, optimal_k)
 
     # Conclude with one GMM for the second? group
 
